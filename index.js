3f36ccbe751a3a48867939401b99e6789f5d0bc2
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-05 03:57:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function doToElementsInArray(array, callback) {array.forEach(callback);}
 function changeCompletely(element, index, array) {
-  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]s!!!}`;
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
 doToElementsInArray(animals, changeCompletely);
 
